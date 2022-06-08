c91ab87acb1b9dca3035f023e3017baf91d02951
olandroveg <olandroveg@gmail.com>
2022-06-08 13:19:26 +0200

    models

---
+++ CoreNRF/CoreNRF/Models/Services.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoreNRF.Models
+{
+    public class Services
+    {
+        public Guid Id { get; set; }
+        public Guid NfId { get; set; }
+        public NF Nf { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string NfBaseAddress { get; set; }
+        public string ServiceAPI { get; set; }
+    }
+}
