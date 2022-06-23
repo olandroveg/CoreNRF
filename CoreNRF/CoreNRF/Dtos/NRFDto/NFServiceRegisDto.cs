d2fbc71ecad8233dd8abf726bcfa333f41ded615
olandroveg <olandroveg@gmail.com>
2022-06-23 13:23:59 +0200

    1

---
+++ CoreNRF/CoreNRF/Dtos/NRFDto/NFServiceRegisDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CoreNRF.Dtos.NRFDto
+{
+    public class NFServiceRegisDto
+    {
+        public string Id { get; set; }
+        public ICollection< string> ServiceName { get; set; }
+    }
+}
