e3dcca5db2d59128c8c257ac338580597f85eb87
olandroveg <olandroveg@gmail.com>
2022-06-24 13:14:45 +0200

    1

---
+++ CoreNRF/CoreNRF/RequestAPI/NotifySuscription.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreNRF.RequestAPI
+{
+    public class NotifySuscription
+    {
+        private string _NFurl;
+        public NotifySuscription(string NFaddress)
+        {
+            _NFurl = NFaddress;
+        }
+
+    }
+}
