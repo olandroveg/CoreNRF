fff194910fe05ec07317a665bfa88b527bff0230
olandroveg <olandroveg@gmail.com>
2022-11-04 15:18:49 +0100

    1

---
+++ CoreNRF/CoreNRF/Migrations/20221104141445_AddFieldversion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreNRF.Migrations
+{
+    public partial class AddFieldversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Version",
+                table: "NF",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "NF");
+        }
+    }
+}
