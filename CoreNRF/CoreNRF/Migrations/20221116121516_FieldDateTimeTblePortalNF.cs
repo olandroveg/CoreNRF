6b8f889386f0165d128c89aff7b2968fa2da8db9
olandroveg <olandroveg@gmail.com>
2022-11-16 13:16:13 +0100

    1

---
+++ CoreNRF/CoreNRF/Migrations/20221116121516_FieldDateTimeTblePortalNF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreNRF.Migrations
+{
+    public partial class FieldDateTimeTblePortalNF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTime",
+                table: "PortalNF",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTime",
+                table: "PortalNF");
+        }
+    }
+}
--- CoreNRF/CoreNRF/Models/PortalNF.cs
+++ CoreNRF/CoreNRF/Models/PortalNF.cs
@@ -5,11 +5,12 @@ namespace CoreNRF.Models
     public class PortalNF
     {
         public Guid Id { get; set; }
-        //RelationName will be the concatenation of PortalName+NFName
+        //RelationName will be the concatenation of ScourceNFName+TargetNFName
         public string RelationName { get; set; }
         public Guid PortalId { get; set; }
         public Portal Portal { get; set; }
         public Guid NFId { get; set; }
         public NF NF { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
